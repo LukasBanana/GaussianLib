bee7da1f69fe5b40381ab445a7c1a135025e286d
Lukas Hermanns <lukas.hermanns90@gmail.com>
2017-10-08 14:50:10 +0200

    Refactored configuration.

--- include/Gauss/Config.h
+++ include/Gauss/Config.h
@@ -13,19 +13,22 @@
 #   define GS_ENABLE_ASSERT
 #endif
 
+//! Uses double precision floating-points for real types.
+//#define GS_REAL_DOUBLE
+
 //! Uses a runtime exception instead of an assert.
-#define GS_ASSERT_EXCEPTION
+//#define GS_ASSERT_EXCEPTION
 
 //! Enables the swizzle operator in the vector classes. If undefined, swizzle operator is disabled (default).
-#define GS_ENABLE_SWIZZLE_OPERATOR
+//#define GS_ENABLE_SWIZZLE_OPERATOR
 
 //! Enables the inverse matrix operator, i.e. allows "A^-1" expressions as shortcut for "A.Inverse()".
-#define GS_ENABLE_INVERSE_OPERATOR
+//#define GS_ENABLE_INVERSE_OPERATOR
 
 //! Disables automatic data initialization. If undefined, automatic initialization is enabled (default). 
 //#define GS_DISABLE_AUTO_INIT
 
-//! Enables column-major storage. If undefined, column-major storage is used (default).
+//! Enables row-major storage. If undefined, column-major storage is used (default).
 //#define GS_ROW_MAJOR_STORAGE
 
 //! Enables row vectors. If undefined, column vectors are used (default).
